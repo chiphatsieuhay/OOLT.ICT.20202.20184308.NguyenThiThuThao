240db10e923ede1e661c357699febdf8dc4e0d3f
chiphatsieuhay <71662174+chiphatsieuhay@users.noreply.github.com>
2021-02-27 18:35:13 +0700

    Update ChoosingOption.java
    with 2 options ONLY

--- Lab01/ChoosingOption.java
+++ Lab01/ChoosingOption.java
@@ -1,7 +1,14 @@
 import javax.swing.JOptionPane;
 public class ChoosingOption{
     public static void main(String[] args){
+        //exactly the example that teacher gave
         int option = JOptionPane.showConfirmDialog(null,"Do you want to change to the first class ticket");
+        JOptionPane.showMessageDialog(null,"You've chosen: "+ (option == JOptionPane.YES_OPTION?"Yes":"No"));
+
+
+        //With ONLY 2 Choices
+        option = JOptionPane.showConfirmDialog(null,"Do you want to change to the first class ticket (2) choices","WARNING",
+        JOptionPane.YES_NO_OPTION);
         JOptionPane.showMessageDialog(null,"You've chosen: "+ (option == JOptionPane.YES_OPTION?"Yes":"No"));
         System.exit(0);
     }
