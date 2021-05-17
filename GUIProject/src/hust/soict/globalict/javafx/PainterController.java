40e5878be22caed6060eb68e4ef69c9528efb462
Nguyen Thi Thu Thao <thao.chip.0305@gmail.com>
2021-05-17 15:27:50 +0700

    fix

--- GUIProject/src/hust/soict/globalict/javafx/PainterController.java
+++ GUIProject/src/hust/soict/globalict/javafx/PainterController.java
@@ -1,5 +1,37 @@
 package hust.soict.globalict.javafx;
 
-public class PainterController {
+ 
 
+import java.awt.Color;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+
+ 
+
+public class PainterController {
+    @FXML
+    private Pane drawingAreaPane;
+
+ 
+
+    @FXML
+    void clearButtonPressed(ActionEvent event) {
+        drawingAreaPane.getChildren().clear();
+    }
+
+ 
+
+    @FXML
+    void drawingAreaMouseDragged(MouseEvent event) {
+        Circle newCircle = new Circle(event.getX(),event.getY(),4);
+       
+        drawingAreaPane.getChildren().add(newCircle);
+    }
+    
+    
 }
